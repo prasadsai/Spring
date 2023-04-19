db204407b254b9dabba899bdcbbdba7035634684
prasadsai <prasadsai0369@gmail.com>
2023-04-19 19:19:53 -0400

    04-qualifiers folder is created

---
+++ dev-spring-boot/02-spring-boot-spring-core/04-qualifiers/src/main/java/com/prasad/springcoredemo/rest/DemoController.java
@@ -0,0 +1,27 @@
+package com.prasad.springcoredemo.rest;
+
+import com.prasad.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    //define a private field for the dependency
+    private Coach myCoach;
+
+    //define a constructor for dependency injection
+//    @Autowired //tells spring to inject a dependency
+//    public DemoController(Coach theCoach){
+//        this.myCoach=theCoach;
+//    }
+    @Autowired
+    public void setCoach(Coach theCoach){
+        this.myCoach=theCoach;
+    }
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkout();
+    }
+}
