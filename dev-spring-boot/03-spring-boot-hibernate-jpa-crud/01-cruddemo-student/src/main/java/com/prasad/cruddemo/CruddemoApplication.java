484e8c8515f8d10e7d96cd22b5d255c9bf9ad235
prasadsai <prasadsai0369@gmail.com>
2023-04-25 16:48:16 -0400

    01-cruddemo-student CommandLineRunner is set up

--- dev-spring-boot/03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/prasad/cruddemo/CruddemoApplication.java
+++ dev-spring-boot/03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/prasad/cruddemo/CruddemoApplication.java
@@ -1,13 +1,23 @@
 package com.prasad.cruddemo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CruddemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CruddemoApplication.class, args);
+	}
+
+	@Bean   //This snippet of code will execute after the Spring beans are created
+	public CommandLineRunner commandLineRunner(String[] args){ //CommandLineRunner is from the Spring Boot Framework
+		//lambda expression
+		return runner->{
+			System.out.println("Hello World!");
+		};
 	}
 
 }
