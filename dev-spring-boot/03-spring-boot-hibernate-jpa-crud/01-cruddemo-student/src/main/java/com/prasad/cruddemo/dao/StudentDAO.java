018f157b96ac2b90c554dc78ab1db262df647494
prasadsai <prasadsai0369@gmail.com>
2023-04-26 20:08:13 -0400

    01-cruddemo-student: Created StudentDAO interface and implementation for it in StudentDAOImpl, with entitymanager field, constructor injection, and transactional for database save method

---
+++ dev-spring-boot/03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/prasad/cruddemo/dao/StudentDAO.java
@@ -0,0 +1,8 @@
+package com.prasad.cruddemo.dao;
+
+import com.prasad.cruddemo.entity.Student;
+
+public interface StudentDAO {
+    void save(Student theStudent);
+
+}
---
+++ dev-spring-boot/03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/prasad/cruddemo/dao/StudentDAOImpl.java
@@ -0,0 +1,35 @@
+package com.prasad.cruddemo.dao;
+
+import com.prasad.cruddemo.entity.Student;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository // gives us Support for Component-scanning, also handle the spring support for exception translation.
+public class StudentDAOImpl implements StudentDAO{
+
+    //define field for entity manager
+    private EntityManager entityManager;
+
+    //constructor to inject entity manager using constructor injection
+    @Autowired
+    public StudentDAOImpl(EntityManager entityManager){
+        this.entityManager = entityManager;
+    }
+
+    //implement save method
+    @Override
+    @Transactional // as we are updating the database
+    public void save(Student theStudent) {
+        entityManager.persist(theStudent); // saves the student into the database.
+    }
+}
+
+
+
+
+
+
+
+
