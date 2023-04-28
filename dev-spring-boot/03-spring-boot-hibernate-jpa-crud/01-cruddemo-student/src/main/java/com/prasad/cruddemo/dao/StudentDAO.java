0c202a30ae84249dafdd717bc9f9b676a2c575e9
prasadsai <prasadsai0369@gmail.com>
2023-04-27 21:07:04 -0400

    01-cruddemo-student: performed retrieve in the interface, implementing the method and finally updating the app in the CruddemoApplication.

--- dev-spring-boot/03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/prasad/cruddemo/dao/StudentDAO.java
+++ dev-spring-boot/03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/prasad/cruddemo/dao/StudentDAO.java
@@ -2,10 +2,14 @@ package com.prasad.cruddemo.dao;
 
 import com.prasad.cruddemo.entity.Student;
 
+import java.util.List;
+
 public interface StudentDAO {
     void save(Student theStudent);
 
     Student findById(Integer id); // why Integer? why not int?
+
+    List<Student> findAll();
 
 
 
--- dev-spring-boot/03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/prasad/cruddemo/dao/StudentDAOImpl.java
+++ dev-spring-boot/03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/prasad/cruddemo/dao/StudentDAOImpl.java
@@ -2,9 +2,12 @@ package com.prasad.cruddemo.dao;
 
 import com.prasad.cruddemo.entity.Student;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository // gives us Support for Component-scanning, also handle the spring support for exception translation.
 public class StudentDAOImpl implements StudentDAO{
@@ -28,6 +31,14 @@ public class StudentDAOImpl implements StudentDAO{
     @Override
     public Student findById(Integer id) { // why not int and why Integer?
         return entityManager.find(Student.class, id);
+    }
+
+    @Override
+    public List<Student> findAll() {
+        // create query
+        TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student order by lastName", Student.class);
+        // return query results
+        return theQuery.getResultList();
     }
 }
 
